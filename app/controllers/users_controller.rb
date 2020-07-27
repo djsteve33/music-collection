79ac5f0568b8c012ba8a12ae2be14c9ea2b2ce79
djsteve33 <swomble@aol.com>
2020-07-27 17:58:18 -0400

    adding methods to MusicCollectionController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+    # index action
     get '/users' do
         @users = User.all 
         erb :index
+    end
+
+    # show action
+    get 'users/:id' do
+        @user = User.find_by_id(params[:id])
+        erb :'/show'
     end
 end
