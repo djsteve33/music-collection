c71645dfebf0129558cbfecfee6c8f7d6ef3380f
djsteve33 <swomble@aol.com>
2020-08-04 21:33:03 -0400

    updating file/folder structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
     # index action
-    get '/users' do
-        @users = User.all 
-        erb :index
-    end
+    #get '/users' do
+     #   @users = User.all 
+      #  erb :index
+  #  end
 
     # show action
-    get 'users/:id' do
-        @user = User.find_by_id(params[:id])
-        erb :'/show'
-    end
+  #  get 'users/:id' do
+       # @user = User.find_by_id(params[:id])
+      #  erb :'/show'
+   # end
 end
--- config.ru
+++ config.ru
@@ -5,5 +5,5 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 use UsersController
-use AlbumController
+use AlbumsController
 run ApplicationController
