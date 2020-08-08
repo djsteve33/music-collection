94729d2235cbb41d567b7e7b64ac9717dcef5179
djsteve33 <swomble@aol.com>
2020-08-08 19:15:26 -0400

    added additional routes in users controller, updated index.erb, added failure.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,22 +12,43 @@ class UsersController < ApplicationController
    # end
 
    get '/signup' do
-    if logged_in?
-      erb :'users/create_user'
-      #locals: (message: "Please sign up before you sign in")
+      erb :'users/create_user'    
+  end
+
+  post '/signup' do
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      if  @user.save
+      session[:user_id] = @user.id
+      redirect to '/login'
+      else
+        redirect to '/failure'
+      end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
     else
       redirect to '/albums'
     end
   end
 
-  post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect to '/signup'
+  post '/login' do
+  user = User.find_by(:username => params[:usernamer])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+     redirect to '/albums'
     else
-      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @user.save
-      session[:user_id] = @user.id
-      redirect to '/albums'
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
     end
   end
 end
